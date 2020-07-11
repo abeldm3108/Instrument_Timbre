3c829a952c318cd1cbaaf04c5dbd296d522349c8
abeldm3108 <abeldm3108@gmail.com>
2020-07-11 17:15:50 +0200

    add sliders

--- Sliders.py
+++ Sliders.py
@@ -1,43 +1,49 @@
+from matplotlib.widgets import Slider  # import the Slider widget
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
+from music import * 
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.25, bottom=0.25)
-t = np.arange(0.0, 1.0, 0.001)
-a0 = 5
-f0 = 3
-s = a0 * np.sin(2 * np.pi * f0 * t)
-l, = plt.plot(t, s, lw=2)
-ax.margins(x=0)
+armonics = [0,0,0,0,0,0,0,0]
 
-axcolor = 'lightgoldenrodyellow'
-axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+slider_min = 0    # the minimial value of the paramater a
+slider_max = 1   # the maximal value of the paramater a
+slider_init = 0   # the value of the parameter a to be used initially, when the graph is created
 
-sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+def update(a):
+    armonics[a]=
+    #sin_plot.set_ydata(np.sin(a*x)) # set new y-coordinates of the plotted points
+    #fig.canvas.draw_idle()          # redraw the plot
+
+class ArmonicSlider:
+    '''
+    slider_1_plt = plt.axes([0.15, 0., 0.8, 0.05]) #Left margin / bottom margin / widht / height
+
+    # here we create the slider
+    slider_1 = Slider(slider_1_plt,      # the axes object containing the slider
+                    '1st armonic',  # the name of the slider parameter
+                    slider_min,          # minimal value of the parameter
+                    slider_max,          # maximal value of the parameter
+                    valinit=slider_init  # initial value of the parameter
+                    )
+    '''
+    slider_plt = []
+    slider = []
+    for i in range(4):
+        slider_plt.append(plt.axes([0.15, 0.9-0.1*i, 0.8, 0.05]))
+        slider.append(Slider(slider_plt[i], " armonic", slider_min, slider_max, valinit=slider_init))
+        slider[i].on_changed(update)
+
+# Next we define a function that will be executed each time the value
+# indicated by the slider changes. The variable of this function will
+# be assigned the value of the slider.
 
 
-def update(val):
-    amp = samp.val
-    freq = sfreq.val
-    l.set_ydata(amp*np.sin(2*np.pi*freq*t))
-    fig.canvas.draw_idle()
-
-sfreq.on_changed(update)
-samp.on_changed(update)
-
-resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-resetButton = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+# the final step is to specify that the slider needs to
+# execute the above function when its value changes
+#a_slider.on_changed(update)
 
 
-def reset(event):
-    sfreq.reset()
-    samp.reset()
-resetButton.on_clicked(reset)
 
-rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
-
+playnote(hz=220, , time=10, intensity = 0.3)
 
 plt.show()
