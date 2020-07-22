df3d4f40e986ee08f625bd13aeb7ee9a7193c3f4
abeldm3108 <abeldm3108@gmail.com>
2020-07-22 13:16:08 +0200

    .

--- Sliders.py
+++ Sliders.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from playfrecuency import *
+from playfrequency import *
 import numpy as np
 import sounddevice as sd
 
@@ -26,20 +26,26 @@ label.pack()
 def updatelabelinfo1(val):
    harmonic[0]=val
    sd.stop()
-   playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1, fr=fr_def)
+   playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1,loop=True)
 def updatelabelinfo2(val):
    harmonic[1]=val
    sd.stop()
-   playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1, fr=fr_def)
+   playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1,loop=True)
 def updatelabelinfo3(val):
    harmonic[2]=val
    sd.stop()
-   playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1, fr=fr_def)
+   playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1,loop=True)
 
 #Creating sliders
-slider1 = Scale(root, label="Harmonic 1", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo1).pack(side=LEFT)
-slider2 = Scale(root, label="Harmonic 2", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo2).pack(side=LEFT)
-slider3 = Scale(root, label="Harmonic 3", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo3).pack(side=LEFT)
+slider1 = Scale(root, label="Harmonic 1", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo1)
+slider1.pack(side=LEFT)
+slider1.set(5)
+slider2 = Scale(root, label="Harmonic 2", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo2)
+slider2.pack(side=LEFT)
+slider2.set(5)
+slider3 = Scale(root, label="Harmonic 3", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo3)
+slider3.pack(side=LEFT)
+slider3.set(5)
 
 
 '''
--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 from playfrequency import *
 
-playnote(freq=220, instrument=horn1 , duration=7, intensity = 0.6)
-playnote(freq=220, instrument=horn2 , duration=7, intensity = 0.6)
 
-playnote(freq=220, instrument=horn1 , duration=7, intensity = 0.6, loop=True)
+playnote(freq=220, instrument=horn2, intensity = 1.5, loop=True)
 
 i=0
 while i<10:
-    print(i)
+    if input()=="stop":
+        sd.stop()
+        playnote(freq=110, instrument=horn1, intensity = 1.5, duration=3)
+        break
     time.sleep(1)
     i=i+1
+
+j=0
+while j<3:
+    time.sleep(1)
+    j=j+1
+
+
