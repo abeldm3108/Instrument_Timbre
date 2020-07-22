bc6a83012e77f219dc145639d7ce7755b6ee0e53
abeldm3108 <abeldm3108@gmail.com>
2020-07-22 17:22:33 +0200

    .

--- Sliders.py
+++ Sliders.py
@@ -14,33 +14,36 @@ freq_def=220
 
 #Array of intensity of harmonics
 harmonic = np.zeros(numberofharmonics)
-
+#slider_val = np.empty(numberofharmonics, DoubleVar())
 
 #Creating update function of sliders
 def updatelabelinfo1(val):
-   harmonic[0]=val   
-   labelvar.set(str(harmonic[0])+"   " +str(harmonic[1])+"   " +str(harmonic[2]))
+   harmonic[0]=slider1_val.get()
+   labelvar.set(str(slider1_val.get())+"   " +str(slider2_val.get())+"   " +str(slider3_val.get()))
    sd.stop()
    playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1,loop=True)
 def updatelabelinfo2(val):
-   harmonic[1]=val
-   labelvar.set(str(harmonic[0])+"   " +str(harmonic[1])+"   " +str(harmonic[2]))
+   harmonic[1]=slider2_val.get()
+   labelvar.set(str(slider1_val.get())+"   " +str(slider2_val.get())+"   " +str(slider3_val.get()))
    sd.stop()
    playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1,loop=True)
 def updatelabelinfo3(val):
-   harmonic[2]=val
-   labelvar.set(str(harmonic[0])+"   " +str(harmonic[1])+"   " +str(harmonic[2]))
+   harmonic[2]=slider3_val.get()
+   labelvar.set(str(slider1_val.get())+"   " +str(slider2_val.get())+"   " +str(slider3_val.get()))
    sd.stop()
    playnote(freq=freq_def, duration=maxtime, instrument=harmonic, intensity=1,loop=True)
 
 #Creating sliders
-slider1 = Scale(root, label="Harmonic 1", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo1)
+slider1_val=DoubleVar()
+slider2_val=DoubleVar()
+slider3_val=DoubleVar()
+slider1 = Scale(root, label="Harmonic 1",variable=slider1_val, tickinterval=0.25 , from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo1)
 slider1.pack(side=LEFT)
 slider1.set(5)
-slider2 = Scale(root, label="Harmonic 2", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo2)
+slider2 = Scale(root, label="Harmonic 2",variable=slider2_val, from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo2)
 slider2.pack(side=LEFT)
 slider2.set(5)
-slider3 = Scale(root, label="Harmonic 3", from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo3)
+slider3 = Scale(root, label="Harmonic 3", variable=slider3_val, from_=sliderminval, to=slidermaxval, length=sliderlength, width= sliderwidth, command=updatelabelinfo3)
 slider3.pack(side=LEFT)
 slider3.set(5)
 
